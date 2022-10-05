17486a8a08f41c3fa3b5aa019dc197334fb72ee0
rajiv <gyawalirajiv@gmail.com>
2022-10-05 18:26:52 -0500

    Initial Commit

---
+++ src/test/java/com/example/springbatchtodb/SpringBatchToDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchtodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchToDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
